7447ae8f27b9233df922620fb61e9774819b5fdc
yueyang1101 <43494388+yueyang1101@users.noreply.github.com>
2020-03-10 14:56:39 -0700

    Add files via upload

---
+++ src/plots/Plot_Trade_CO2_Heatmap.py
@@ -0,0 +1,25 @@
+import numpy as np
+import pandas as pd
+import holoviews as hv
+import hvplot.pandas
+hv.extension('bokeh')
+
+
+def plot_TradeCO2_heatmap(tradedata, feature):
+    '''
+    Plot the heat map
+    :param tradedata: a processed data frame
+    :param feature: the vairable for the plot
+    :return: a heatmap lpot
+    '''
+    assert isinstance(tradedata, pd.DataFrame)
+    assert isinstance(varaiable, str)
+    
+    edata = hv.Dataset(data =tradedata,kdims=['Entity','Year'])
+    
+    if feature == 'netCO2':
+        %%opts HeatMap [colorbar=True,width=600,height=500,xrotation=60,tools=['hover'], symmetric=True ]( cmap='RdYlBu')
+    else: 
+        %%opts HeatMap [colorbar=True,width=600,height=500,xrotation=60,tools=['hover'], symmetric=True ]( cmap='RdYlBu')
+    
+    return edata.to(hv.HeatMap,['Year','Entity'],feature, label = feature)
