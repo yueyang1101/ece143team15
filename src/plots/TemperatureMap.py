4a5785ed66390110508e18160c6b4d4da99be5f8
oliver_li <oliver_li@yahoo.com>
2020-03-12 11:23:59 -0700

    merge temperature heatmap to master branch

--- src/plots/TemperatureMap.py
+++ src/plots/TemperatureMap.py
@@ -10,13 +10,13 @@ import imageio
 from IPython.display import HTML
 import random
 
-shapefile = 'VisualizationData/CountryShapes/ne_110m_admin_0_countries.shp'
+shapefile = 'Data/OriginalData/CountryShapes/ne_110m_admin_0_countries.shp'
 # Read shapefile using Geopandas
 gdf = gpd.read_file(shapefile)[['ADMIN', 'ADM0_A3', 'geometry']]
 # Rename columns.
 gdf.columns = ['country', 'country_code', 'geometry']
 # import temperature dataset
-datafile = 'OriginalData/hadcrut-surface-temperature-anomaly.csv'
+datafile = 'Data/OriginalData/hadcrut-surface-temperature-anomaly.csv'
 df = pd.read_csv(datafile, names=['country', 'code', 'year', 'temperature'], skiprows=1)
 
 
