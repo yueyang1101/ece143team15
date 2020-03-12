0ab9bce2b9dbc68ada7d42c2e03525fc121ac765
Anjuman Raha <AnjumanR@users.noreply.github.com>
2020-03-12 04:03:30 -0700

    Data processing for 1.5 Degrees Mitigation Data

---
+++ src/DataProcessing/process_1.5.py
@@ -0,0 +1,26 @@
+def process1_5(df15):
+	'''
+	This function takes a data frame file and returns a dataframe of 
+	historical carbon emissions data and a dataframe with migitation 
+	values from 2000-2026 for maximum +1.5 Degrees warming.
+	:param
+		Input: df15 --> The dataframe of the orginal data in the file
+						mitigation_curves_1.5C_191203_data.csv which 
+						contains historical carbon emissoins data and 
+						mitigation curves for 1.5 degrees
+		Output: a list with just the historical data dataframe and 
+				mitigation curves data in a dataframe
+	'''
+	assert isinstance(df15,pd.DataFrame)
+	import pandas as pd
+
+	indexNames = df15[ df15['Year'] < 1900 ].index
+	df15.drop(indexNames , inplace=True)
+
+	df_historical_data = df15[['Year','Historical']]
+
+	df_historical_data.to_csv('Historical_CO2_Emissions_Data.csv')
+
+	df15.to_csv('1.5_Mitigation_Curves_Data.csv')
+
+	return [df_historical_data, df15]
