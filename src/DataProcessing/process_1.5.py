1fd05424c1f02f5a0bedd719f0fe067bd26839d6
Anjuman Raha <AnjumanR@users.noreply.github.com>
2020-03-13 12:58:20 -0700

    Update files paths

--- src/DataProcessing/process_1.5.py
+++ src/DataProcessing/process_1.5.py
@@ -1,4 +1,5 @@
-def process1_5(df15):
+import pandas as pd
+def process1_5(df15 = pd.read_csv("Data/OriginalData/mitigation_curves_1.5C_191203_data.csv")):
 	'''
 	This function takes a data frame file and returns a dataframe of 
 	historical carbon emissions data and a dataframe with migitation 
@@ -12,15 +13,16 @@ def process1_5(df15):
 				mitigation curves data in a dataframe
 	'''
 	assert isinstance(df15,pd.DataFrame)
-	import pandas as pd
+
 
 	indexNames = df15[ df15['Year'] < 1900 ].index
 	df15.drop(indexNames , inplace=True)
 
 	df_historical_data = df15[['Year','Historical']]
 
-	df_historical_data.to_csv('Historical_CO2_Emissions_Data.csv')
+	df_historical_data.to_csv('Data/FinalData/Historical_CO2_Emissions_Data.csv')
 
-	df15.to_csv('1.5_Mitigation_Curves_Data.csv')
+	df15.to_csv('Data/FinalData/1.5_Mitigation_Curves_Data.csv')
 
 	return [df_historical_data, df15]
+process1_5()
--- src/DataProcessing/process_2.py
+++ src/DataProcessing/process_2.py
@@ -1,20 +1,21 @@
-def process2(df2):
+import pandas as pd
+def process2(df2 = pd.read_csv("Data/OriginalData/mitigation_curves_2.0C_191203_data.csv")):
 	'''
 	This function takes a data frame file and returns a dataframe 
-	with migitation values from 2000-2026 for maximum +1.5 Degrees 
+	with migitation values from 2000-2026 for maximum +2 Degrees 
 	warming.
 	:param
 		Input: df2 --> The dataframe of the orginal data in the file
-						mitigation_curves_1.5C_191203_data.csv which 
-						contains historical carbon emissoins data and 
-						mitigation curves for 1.5 degrees
+						mitigation_curves_2.0C_191203_data.csv which 
+						contains mitigation curves for 2 degrees
 		Output: returns a mitigation curves data in a dataframe 
 	'''
 	assert isinstance(df2,pd.DataFrame)
-	import pandas as pd
+
 
 	indexNames = df2[ df2['Year'] < 1900 ].index
 	df2.drop(indexNames , inplace=True)
 
 
-	df2.to_csv('2_Mitigation_Curves_Data.csv')
+	df2.to_csv('Data/FinalData/2_Mitigation_Curves_Data.csv')
+process2()
